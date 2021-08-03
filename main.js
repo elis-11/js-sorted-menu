2d79cfe16c4635abfee3dc190a97ee18648679ba
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-08-03 19:20:19 +0200

    {         name: 'Laptop Apple MacBook Pro 13 i5 2.3/8/128Gb SG',         cost: 1533,         rated: 4.5,         img: 'https://github.com/egormolchanov/sort-goods/blob/master/image/laptop2.jpg?raw=true'     },     {         name: 'Laptop HP Pavilion 13-an0072ur 5WC37EA',         cost: 705,         rated: 4.1,         img: 'https://github.com/egormolchanov/sort-goods/blob/master/image/laptop4.jpg?raw=true'     },

--- main.js
+++ main.js
@@ -11,3 +11,15 @@ const listGoods = [
         rated: 4.6,
         img: 'https://github.com/egormolchanov/sort-goods/blob/master/image/laptop3.jpg?raw=true'
     },
+    {
+        name: 'Laptop Apple MacBook Pro 13 i5 2.3/8/128Gb SG',
+        cost: 1533,
+        rated: 4.5,
+        img: 'https://github.com/egormolchanov/sort-goods/blob/master/image/laptop2.jpg?raw=true'
+    },
+    {
+        name: 'Laptop HP Pavilion 13-an0072ur 5WC37EA',
+        cost: 705,
+        rated: 4.1,
+        img: 'https://github.com/egormolchanov/sort-goods/blob/master/image/laptop4.jpg?raw=true'
+    },
